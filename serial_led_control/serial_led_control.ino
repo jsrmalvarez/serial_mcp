cc653462b5988cac58a2592be186b5125b6d2c47
José Ramón Álvarez Rojo <joseramon.alvarez@betterask.erni>
2025-06-04 12:27:00 +0200

    Can control LED and read status

--- serial_led_control/serial_led_control.ino
+++ serial_led_control/serial_led_control.ino
@@ -1,9 +1,44 @@
+#include <Arduino.h>
+
+bool ledState = false;
+String inputString = "";
+bool stringComplete = false;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  inputString.reserve(200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (stringComplete) {
+    if (inputString == "led_on\n") {
+      digitalWrite(LED_BUILTIN, HIGH);
+      ledState = true;
+      Serial.print("ack\n");
+    }
+    else if (inputString == "led_off\n") {
+      digitalWrite(LED_BUILTIN, LOW);
+      ledState = false;
+      Serial.print("ack\n");
+    }
+    else if (inputString == "get_led\n") {
+      Serial.print(ledState ? "on\n" : "off\n");
+    }
+    
+    inputString = "";
+    stringComplete = false;
+  }
+}
 
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
 }
